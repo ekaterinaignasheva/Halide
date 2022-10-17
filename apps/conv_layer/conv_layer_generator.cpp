0e94b5b0a713e9d26388c058d265d6e00d0d92e4
Ekaterina Ignasheva <eignasheva@google.com>
2022-10-17 13:38:28 -0700

    Implement the first version of the conv2d with Halide

--- apps/conv_layer/conv_layer_generator.cpp
+++ apps/conv_layer/conv_layer_generator.cpp
@@ -1,4 +1,5 @@
 #include "Halide.h"
+#include <iostream>
 
 namespace {
 
@@ -7,198 +8,55 @@ using namespace Halide;
 class ConvolutionLayer : public Halide::Generator<ConvolutionLayer> {
 public:
     Input<Buffer<float, 4>> input{"input"};
-    Input<Buffer<float, 4>> filter{"filter"};
-    Input<Buffer<float, 1>> bias{"bias"};
-    Output<Buffer<float, 4>> relu{"relu"};
+    // t_source, k_destination, source_depth, window_h, window_w, destination_depth
+    Input<Buffer<float, 6>> weights{"weights"};
+    Input<Buffer<float, 2>> bias{"bias"};
+    Output<Buffer<float, 4>> output{"output"};
 
     void generate() {
-        const int N = 5, CI = 128, CO = 128, W = 100, H = 80;
+        const int SD = 32, DD = 16;
+        const int H = 256, W = 256;
+        const int WG_X = 4, WG_Y = 4;
+        const int window = 3;
 
-        /* THE ALGORITHM */
+        Var t("t"), h("h"), w("w"), d("d");
+        Func conv("conv"), padded;
 
-        Var x("x"), y("y"), c("c"), n("n");
+        RDom r(0, 4, 0, SD, -1, window-1);
 
-        Func conv("conv");
-        RDom r(0, CI, 0, 3, 0, 3);
+        padded(t, h, w, d) = input(t, clamp(h, 0, H - 1),
+                                clamp(w, 0, W - 1), d);
+        conv(t, h, w, d) = bias(t, d);
+        conv(t, h, w, d) += padded(r.x, h + r.z, w + r.z, r.y) *
+                            weights(r.x, t, r.y, r.z + 1, r.z + 1, d);
+        output(t, h, w, d) = max(0, conv(t, h, w, d));
 
-        conv(c, x, y, n) = bias(c);
-        conv(c, x, y, n) += filter(c, r.y, r.z, r.x) * input(r.x, x + r.y, y + r.z, n);
+        input.dim(0).set_bounds(0, 4).set_stride(1);
+        input.dim(1).set_bounds(0, H).set_stride(4);
+        input.dim(2).set_bounds(0, W).set_stride(4*H);
+        input.dim(3).set_bounds(0, SD).set_stride(4*H*W);
 
-        relu(c, x, y, n) = max(0, conv(c, x, y, n));
+        weights.dim(0).set_bounds(0, 4).set_stride(1); // t_source
+        weights.dim(1).set_bounds(0, 4).set_stride(4); // k_destination
+        weights.dim(2).set_bounds(0, SD).set_stride(16);// source_depth
+        weights.dim(3).set_bounds(0, window).set_stride(16*SD);// window_h
+        weights.dim(4).set_bounds(0, window).set_stride(16*SD*window);// window_w
+        weights.dim(5).set_bounds(0, DD).set_stride(16*SD*window*window);// destination_depth
 
-        /* THE SCHEDULE */
+        bias.dim(0).set_bounds(0, 4).set_stride(1);
+        bias.dim(1).set_bounds(0, DD).set_stride(4);
 
-        // MKL JITs code for the specific size and strides, so we'll
-        // do the same and ask Halide to compile for this specific
-        // size:
+        output.dim(0).set_bounds(0, 4).set_stride(1);
+        output.dim(1).set_bounds(0, H).set_stride(4);
+        output.dim(2).set_bounds(0, W).set_stride(4*H);
+        output.dim(3).set_bounds(0, DD).set_stride(4*H*W);
 
-        relu.dim(0).set_bounds(0, CO).set_stride(1);
-        relu.dim(1).set_bounds(0, W).set_stride(CO);
-        relu.dim(2).set_bounds(0, H).set_stride(CO * W);
-        relu.dim(3).set_bounds(0, N).set_stride(CO * H * W);
+        // conv.update(0).atomic().vectorize(r.x);
 
-        input.dim(0).set_bounds(0, CI).set_stride(1);
-        input.dim(1).set_bounds(0, W + 2).set_stride(CI);
-        input.dim(2).set_bounds(0, H + 2).set_stride(CI * (W + 2));
-        input.dim(3).set_bounds(0, N).set_stride(CI * (W + 2) * (H + 2));
-
-        filter.dim(0).set_bounds(0, CO).set_stride(1);
-        filter.dim(1).set_bounds(0, 3).set_stride(CO);
-        filter.dim(2).set_bounds(0, 3).set_stride(CO * 3);
-        filter.dim(3).set_bounds(0, CI).set_stride(CO * 3 * 3);
-
-        bias.dim(0).set_bounds(0, CO).set_stride(1);
-
-        if (using_autoscheduler()) {
-            input.dim(0).set_estimate(0, CI);
-            input.dim(1).set_estimate(0, W + 2);
-            input.dim(2).set_estimate(0, H + 2);
-            input.dim(3).set_estimate(0, N);
-
-            filter.dim(0).set_estimate(0, CO);
-            filter.dim(1).set_estimate(0, 3);
-            filter.dim(2).set_estimate(0, 3);
-            filter.dim(3).set_estimate(0, CI);
-
-            bias.dim(0).set_estimate(0, CO);
-
-            relu.dim(0).set_estimate(0, W);
-            relu.dim(1).set_estimate(0, H);
-            relu.dim(2).set_estimate(0, CO);
-            relu.dim(3).set_estimate(0, N);
-
-        } else if (get_target().has_feature(Target::CUDA)) {
-            // GPU schedule, tuned for a GTX 980. Seems to be good on
-            // an RTX 2060 too (About 90% peak flops on both cards).
-
-            // 1.87 ms on an RTX 2060. According to NVIDIA Nsight
-            // Compute we're at 91.5% utilization of the FMA units
-
-            // 2.41 ms on a GTX 980. According to nvprof this is about
-            // 88% of peak flops.
-
-            // We use cuda-specific scheduling directives (gpu_lanes),
-            // so this is not a general GPGPU schedule.
-
-            Var ni, no, xi, xo, yi, yo, ci, co, t;
-            RVar rxo, rxi, rxii;
-            relu.compute_root()
-                .split(x, xo, xi, 5)
-                .split(y, yo, yi, 5)
-                .split(c, co, ci, 32)
-                .reorder(xi, yi, ci, xo, yo, co, n)
-                .gpu_lanes(ci)
-                .unroll(xi)
-                .unroll(yi)
-                .fuse(co, n, t)
-                .gpu_blocks(xo, yo, t);
-
-            conv.compute_at(relu, xo)
-                .store_in(MemoryType::Register)
-                .gpu_lanes(c)
-                .unroll(x)
-                .unroll(y)
-                .update()
-                .split(r.x, rxo, rxi, 16)
-                .split(rxi, rxi, rxii, 2)
-                .reorder(c, rxii, x, y, r.y, r.z, rxi, rxo)
-                .gpu_lanes(c)
-                .unroll(x)
-                .unroll(y)
-                .unroll(r.y)
-                .unroll(r.z)
-                .unroll(rxii);
-
-            input.in()
-                .compute_at(conv, rxo)
-                .vectorize(_0, 2)
-                .split(_1, xo, xi, 4)
-                .fuse(_0, xi, t)
-                .gpu_lanes(t)
-                .unroll(xo)
-                .unroll(_2);
-
-        } else {
-
-            // 4.06ms on an Intel i9-9960X using 16 threads at 3.0 GHz,
-            // which is 94.5% of peak flops assuming the math below is correct:
-
-            // 16 cores times 2 FMAs per cycle times 3G cycles per
-            // second times 16 vector lanes is a peak throughput of
-            // 1.536 TFlops.
-
-            // This conv does N * CI * CO * W * H * 3 * 3 = 5 * 128 *
-            // 128 * 100 * 80 * 3 * 3 FMAs in 4.06ms is 1.453 TFlops.
-
-            // The ratio of actual to theoretical flops hit is 0.9458
-
-            int tile_w = 1;
-            int tile_h = 1;
-            const int vec = natural_vector_size<float>();
-
-            if (get_target().has_feature(Target::AVX512_Skylake) ||
-                (get_target().arch == Target::ARM &&
-                 get_target().bits == 64)) {
-                // On Skylake we have one load per fma and 32
-                // registers available, so there's considerable
-                // flexibility in the schedule. We'll use 20 accumulator
-                // registers in a 4x5 tile. This is also a reasonable
-                // choice for ARMv8, which also has 32 registers.
-                tile_w = 4;
-                tile_h = 5;
-            } else if (get_target().arch == Target::X86) {
-                // With 16-register ISAs like x86 with AVX2, we can
-                // only do one load per two fmas, which constrains the
-                // schedule to have to be a squarish 12-register tile
-                // of the output.
-                tile_w = 3;
-                tile_h = 4;
-            } else {
-                // The above should also be reasonable schedule for
-                // ARMv7 and other 16-register machines, but I see
-                // some spills on arm-32, so we use a 2x4 block of 8
-                // accumulators instead. This could probably be better
-                // tuned, because in principle 12 accumulators should
-                // be possible. I believe the issue is that there's no
-                // fused multiply-add instruction, and so we're
-                // fighting llvm's instruction scheduler, which wants
-                // to move the muls well ahead of the adds to cover
-                // instruction latencies.
-                tile_w = 2;
-                tile_h = 4;
-            }
-
-            Var co, ci, xo, xi, yo, yi, t;
-            relu.split(c, co, ci, vec * tile_w)
-                .split(x, xo, xi, tile_h)
-                .reorder(ci, xi, xo, y, n, co)
-                .vectorize(ci, vec)
-                .unroll(ci)
-                .unroll(xi)
-                .parallel(y)
-                .parallel(n)
-                .parallel(co);
-            conv.compute_at(relu, xo)
-                .vectorize(c, vec)
-                .unroll(c)
-                .unroll(x)
-                .unroll(y)
-                .update()
-                .reorder(c, x, y, r.x, r.y, r.z, n)
-                .vectorize(c, vec)
-                .unroll(c)
-                .unroll(x)
-                .unroll(y)
-                .unroll(r.x, 2);
-            filter.in()
-                .compute_at(conv, r.x)
-                .vectorize(_0, vec)
-                .unroll(_0)
-                .unroll(_3);
-            input.in()
-                .compute_at(conv, x)
-                .unroll(_0);
-        }
+        const int WG_Z = 2;
+        Var ho, wo, d_o, hi, wi, di;
+        output.vectorize(t)
+        .gpu_tile(h, w, d, ho, wo, d_o, hi, wi, di, WG_Z, WG_Y, WG_X);
     }
 };
 
--- apps/conv_layer/process.cpp
+++ apps/conv_layer/process.cpp
@@ -1,8 +1,8 @@
 #include <chrono>
 #include <cstdio>
+#include <iostream>
 
 #include "conv_layer.h"
-#include "conv_layer_auto_schedule.h"
 
 #include "HalideBuffer.h"
 #include "halide_benchmark.h"
@@ -11,65 +11,30 @@ using namespace Halide::Tools;
 using namespace Halide::Runtime;
 
 int main(int argc, char **argv) {
-    const int N = 5, CI = 128, CO = 128, W = 100, H = 80;
+    const int T = 4, SD = 32, DD = 16;
+    const int H = 256, W = 256;
+    const int window = 3;
 
-    Buffer<float, 4> input(CI, W + 2, H + 2, N);
-    Buffer<float, 4> filter(CO, 3, 3, CI);
-    Buffer<float, 1> bias(CO);
+    Buffer<float, 4> input(T, H, W, SD);
+    Buffer<float, 6> filter(4, 4, SD, window, window, DD);
+    Buffer<float, 2> bias(T, DD);
+    input.fill(1);
+    filter.fill(1);
+    bias.fill(1);
 
-    for (int c = 0; c < input.dim(3).extent(); c++) {
-        for (int z = 0; z < input.channels(); z++) {
-            for (int y = 0; y < input.height(); y++) {
-                for (int x = 0; x < input.width(); x++) {
-                    input(x, y, z, c) = rand();
-                }
-            }
-        }
-    }
+    Buffer<float, 4> output(T, H, W, DD);
 
-    for (int c = 0; c < filter.dim(3).extent(); c++) {
-        for (int z = 0; z < filter.channels(); z++) {
-            for (int y = 0; y < filter.height(); y++) {
-                for (int x = 0; x < filter.width(); x++) {
-                    filter(x, y, z, c) = rand();
-                }
-            }
-        }
-    }
-
-    for (int x = 0; x < bias.width(); x++) {
-        bias(x) = rand();
-    }
-
-    Buffer<float, 4> output(CO, W, H, N);
-
-// This is necessary to get the PTX compiler to do a good
-// job. TODO: This should be a scheduling directive or a runtime
-// function.
-#ifdef _WIN32
-    _putenv_s("HL_CUDA_JIT_MAX_REGISTERS", "256");
-#else
-    setenv("HL_CUDA_JIT_MAX_REGISTERS", "256", 1);
-#endif
-
+    for (int i =0; i < 20; i++)
     conv_layer(input, filter, bias, output);
 
-    // Timing code
-
     // Manually-tuned version
-    double min_t_manual = benchmark(10, 10, [&]() {
+    double min_t_manual = benchmark(10, 100, [&]() {
         conv_layer(input, filter, bias, output);
         output.device_sync();
     });
     printf("Manually-tuned time: %gms\n", min_t_manual * 1e3);
-
-    // Auto-scheduled version
-    double min_t_auto = benchmark(10, 10, [&]() {
-        conv_layer_auto_schedule(input, filter, bias, output);
-        output.device_sync();
-    });
-    printf("Auto-scheduled time: %gms\n", min_t_auto * 1e3);
-
-    printf("Success!\n");
+    for (int i =0; i < 10; i++){
+      std::cerr << "output " << i << ": " << output.begin()[i] << std::endl;
+    }
     return 0;
 }
